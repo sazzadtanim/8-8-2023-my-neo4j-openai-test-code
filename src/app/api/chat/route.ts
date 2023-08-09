f5cb12e70da1e870c5f229295370e3fbfe2e5dc4
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-09 11:57:42 +0600

    feedMessage type changes

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,3 +1,4 @@
+import { feedMessages } from '@/data/utils'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
@@ -10,10 +11,16 @@ export const runtime = 'edge'
 
 export async function POST(req: Request) {
   const { messages } = await req.json()
+
+  const messageWithInstruction = feedMessages.concat(
+    messages[messages.length - 1]
+  )
+
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages,
+    messages: messageWithInstruction,
+    temperature: 0,
   })
   const stream = OpenAIStream(response)
   return new StreamingTextResponse(stream)
