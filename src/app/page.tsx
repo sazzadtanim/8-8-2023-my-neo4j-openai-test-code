bf5d4682a79a5debbcb09c7e7d5def37093b09a1
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-11 18:11:35 +0600

    added graph by cytoscape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { initialGraphData, layout, styleSheet } from '@/data/data'
 import logo from '@images/oxilate.png'
 import { useChat } from 'ai/react'
 import Image from 'next/image'
+import { useState } from 'react'
+import CytoscapeComponent from 'react-cytoscapejs'
+
 export const runtime = 'edge'
 
 export default function Home() {
@@ -10,8 +14,11 @@ export default function Home() {
     useChat({
       api: '/api/chat',
     })
+  const [width, setWith] = useState('100%')
+  const [height, setHeight] = useState('400px')
 
-  console.log('🚀 ~ file: page.tsx:10 ~ Home ~ messages:', messages)
+  const [graphData, setGraphData] = useState(initialGraphData)
+
   return (
     <main className='flex min-h-screen flex-col bg-black text-black dark:bg-slate-900 dark:text-white'>
       <div className='mb-10 border-b-[1px] border-white/40 p-10'>
@@ -20,6 +27,18 @@ export default function Home() {
       <div className='container mx-auto flex-1'>
         <div className='flex flex-col justify-between'>
           <div className='flex-1'>
+            <CytoscapeComponent
+              elements={CytoscapeComponent.normalizeElements(graphData)}
+              pan={{ x: 200, y: 200 }}
+              style={{ width: width, height: height }}
+              zoomingEnabled={true}
+              maxZoom={3}
+              minZoom={0.1}
+              autounselectify={false}
+              boxSelectionEnabled={true}
+              layout={layout}
+              stylesheet={styleSheet}
+            />
             {isLoading ? (
               <div>...loading</div>
             ) : error ? (
