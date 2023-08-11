c0ead52af7e277b385f556c5d157a2bda7cb5912
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-11 21:25:49 +0600

    added json data from joseph

---
+++ src/data/jsonData.ts
@@ -0,0 +1,584 @@
+import { GraphAPIData } from './typings'
+
+export const myJson: GraphAPIData[] = [
+  {
+    source: {
+      identity: 0,
+      labels: ['Person'],
+      properties: {
+        income: 34661,
+        name: 'Julián Aguirre Mínguez',
+        pagerank: 1,
+        age: 42,
+        hobby: 'Hiking',
+      },
+    },
+    target: {
+      identity: 5,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Toyota Yaris',
+      },
+    },
+    relationship: {
+      identity: 18,
+      start: 0,
+      end: 5,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'CC 7388 AS',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 7,
+      labels: ['Person'],
+      properties: {
+        income: 36349,
+        name: 'Ruperto Jimenez Falcón',
+        pagerank: 1,
+        age: 31,
+        hobby: 'Tennis',
+      },
+    },
+    target: {
+      identity: 10,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Lexus IS',
+      },
+    },
+    relationship: {
+      identity: 25,
+      start: 7,
+      end: 10,
+      type: 'OWNS',
+      properties: {
+        license_plate: '1602 LGR',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 13,
+      labels: ['Person'],
+      properties: {
+        income: 12093,
+        name: 'Germán Huerta Tena',
+        pagerank: 1,
+        age: 19,
+        hobby: 'Drawing',
+      },
+    },
+    target: {
+      identity: 17,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Chevrolet Spark',
+      },
+    },
+    relationship: {
+      identity: 32,
+      start: 13,
+      end: 17,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'GR 4885 EA',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 20,
+      labels: ['Person'],
+      properties: {
+        income: 65866,
+        name: 'Juliana Baena Egea',
+        pagerank: 1,
+        age: 45,
+        hobby: 'Reading',
+      },
+    },
+    target: {
+      identity: 28,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Ford Fiesta',
+      },
+    },
+    relationship: {
+      identity: 39,
+      start: 20,
+      end: 28,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'B 9901 ZK',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 31,
+      labels: ['Person'],
+      properties: {
+        income: 41919,
+        name: 'Mónica Montaña-Santiago',
+        pagerank: 1,
+        age: 31,
+        hobby: 'Reading',
+      },
+    },
+    target: {
+      identity: 35,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Honda Fit',
+      },
+    },
+    relationship: {
+      identity: 46,
+      start: 31,
+      end: 35,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'S 5247 FE',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 38,
+      labels: ['Person'],
+      properties: {
+        income: 7263,
+        name: 'Obdulia Herrera Morante',
+        pagerank: 1,
+        age: 46,
+        hobby: 'Dancing',
+      },
+    },
+    target: {
+      identity: 41,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Fiat 500',
+      },
+    },
+    relationship: {
+      identity: 53,
+      start: 38,
+      end: 41,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'CR 3342 AH',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 43,
+      labels: ['Person'],
+      properties: {
+        income: 30413,
+        name: 'Miguel Ángel Noriega-Sarabia',
+        pagerank: 1,
+        age: 46,
+        hobby: 'Painting',
+      },
+    },
+    target: {
+      identity: 17,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Chevrolet Spark',
+      },
+    },
+    relationship: {
+      identity: 60,
+      start: 43,
+      end: 17,
+      type: 'OWNS',
+      properties: {
+        license_plate: '5836 GCW',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 48,
+      labels: ['Person'],
+      properties: {
+        income: 87864,
+        name: 'Luís Orozco Lopez',
+        pagerank: 1,
+        age: 59,
+        hobby: 'Drawing',
+      },
+    },
+    target: {
+      identity: 5,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Toyota Yaris',
+      },
+    },
+    relationship: {
+      identity: 67,
+      start: 48,
+      end: 5,
+      type: 'OWNS',
+      properties: {
+        license_plate: '8881 HSK',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 51,
+      labels: ['Person'],
+      properties: {
+        income: 19473,
+        name: 'Ignacia Galván Bonet',
+        pagerank: 1,
+        age: 18,
+        hobby: 'Hiking',
+      },
+    },
+    target: {
+      identity: 53,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Mitsubishi Mirage',
+      },
+    },
+    relationship: {
+      identity: 73,
+      start: 51,
+      end: 53,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'GR 4587 EW',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 58,
+      labels: ['Person'],
+      properties: {
+        income: 64952,
+        name: 'Venceslás Mercader Almagro',
+        pagerank: 1,
+        age: 56,
+        hobby: 'Basketball',
+      },
+    },
+    target: {
+      identity: 63,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Hyundai Accent',
+      },
+    },
+    relationship: {
+      identity: 4,
+      start: 58,
+      end: 63,
+      type: 'OWNS',
+      properties: {
+        license_plate: '6673 VSS',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 66,
+      labels: ['Person'],
+      properties: {
+        income: 50787,
+        name: 'Albano Maldonado Ordóñez',
+        pagerank: 1,
+        age: 56,
+        hobby: 'Video games',
+      },
+    },
+    target: {
+      identity: 55,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Toyota 4Runner',
+      },
+    },
+    relationship: {
+      identity: 11,
+      start: 66,
+      end: 55,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'CE 9323 ZK',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 69,
+      labels: ['Person'],
+      properties: {
+        income: 46999,
+        name: 'Esmeralda Sanchez Bou',
+        pagerank: 1,
+        age: 31,
+        hobby: 'Hiking',
+      },
+    },
+    target: {
+      identity: 5,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Toyota Yaris',
+      },
+    },
+    relationship: {
+      identity: 80,
+      start: 69,
+      end: 5,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'LU 8336 ZZ',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 76,
+      labels: ['Person'],
+      properties: {
+        income: 35692,
+        name: 'Pepe Fuster Pujol',
+        pagerank: 1,
+        age: 31,
+        hobby: 'Cycling',
+      },
+    },
+    target: {
+      identity: 79,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Dodge Challenger',
+      },
+    },
+    relationship: {
+      identity: 87,
+      start: 76,
+      end: 79,
+      type: 'OWNS',
+      properties: {
+        license_plate: '1852 WTM',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 81,
+      labels: ['Person'],
+      properties: {
+        income: 96597,
+        name: 'Primitivo Vergara Tejera',
+        pagerank: 1,
+        age: 37,
+        hobby: 'Drawing',
+      },
+    },
+    target: {
+      identity: 85,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Chevrolet Cruze',
+      },
+    },
+    relationship: {
+      identity: 94,
+      start: 81,
+      end: 85,
+      type: 'OWNS',
+      properties: {
+        license_plate: '7366 GFG',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 87,
+      labels: ['Person'],
+      properties: {
+        income: 88176,
+        name: 'Benito Calatayud Avilés',
+        pagerank: 1,
+        age: 48,
+        hobby: 'Painting',
+      },
+    },
+    target: {
+      identity: 89,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Bentley Bentayga',
+      },
+    },
+    relationship: {
+      identity: 101,
+      start: 87,
+      end: 89,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'GR 6406 BC',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 92,
+      labels: ['Person'],
+      properties: {
+        income: 88353,
+        name: 'Inmaculada Alba Fajardo',
+        pagerank: 1,
+        age: 28,
+        hobby: 'Running',
+      },
+    },
+    target: {
+      identity: 35,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Honda Fit',
+      },
+    },
+    relationship: {
+      identity: 108,
+      start: 92,
+      end: 35,
+      type: 'OWNS',
+      properties: {
+        license_plate: '1883 ZMD',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 97,
+      labels: ['Person'],
+      properties: {
+        income: 7498,
+        name: 'Leocadia Múñiz Luz',
+        pagerank: 1,
+        age: 38,
+        hobby: 'Running',
+      },
+    },
+    target: {
+      identity: 101,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Mazda3',
+      },
+    },
+    relationship: {
+      identity: 115,
+      start: 97,
+      end: 101,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'OU 2673 GT',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 103,
+      labels: ['Person'],
+      properties: {
+        income: 7891,
+        name: 'Benito Monreal Aliaga',
+        pagerank: 1,
+        age: 44,
+        hobby: 'Tennis',
+      },
+    },
+    target: {
+      identity: 105,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Chevrolet Sonic',
+      },
+    },
+    relationship: {
+      identity: 121,
+      start: 103,
+      end: 105,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'ML 3730 TN',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 108,
+      labels: ['Person'],
+      properties: {
+        income: 6781,
+        name: 'Pascuala Juan Valbuena',
+        pagerank: 1,
+        age: 48,
+        hobby: 'Painting',
+      },
+    },
+    target: {
+      identity: 112,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Nissan Sentra',
+      },
+    },
+    relationship: {
+      identity: 128,
+      start: 108,
+      end: 112,
+      type: 'OWNS',
+      properties: {
+        license_plate: 'HU 3608 IS',
+      },
+    },
+  },
+  {
+    source: {
+      identity: 114,
+      labels: ['Person'],
+      properties: {
+        income: 56288,
+        name: 'Miriam de Pinto',
+        pagerank: 1,
+        age: 29,
+        hobby: 'Playing Music',
+      },
+    },
+    target: {
+      identity: 116,
+      labels: ['Vehicle'],
+      properties: {
+        model: 'Mazda MX-5 Miata',
+      },
+    },
+    relationship: {
+      identity: 135,
+      start: 114,
+      end: 116,
+      type: 'OWNS',
+      properties: {
+        license_plate: '3644 HNW',
+      },
+    },
+  },
+]
