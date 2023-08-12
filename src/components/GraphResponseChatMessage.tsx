3ec8950618f5172d3f1bcf4949f1f65f60ecf83f
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-12 11:54:15 +0600

    added style type and some style

--- src/components/GraphResponseChatMessage.tsx
+++ src/components/GraphResponseChatMessage.tsx
@@ -26,37 +26,34 @@ export default function GraphResponseChatMessage(props: Props) {
 }
 
 const layout: cytoscape.LayoutOptions = {
-  // fit: true,
-  circle: true,
-  directed: true,
+  name: 'concentric',
+  fit: true,
+  // circle: true,
+  // directed: true,
   padding: 50,
   spacingFactor: 1.5,
   animate: true,
   animationDuration: 1000,
   avoidOverlap: true,
-  nodeDimensionsIncludeLabels: false,
-  name: 'breadthfirst',
+  // nodeDimensionsIncludeLabels: false,
 }
 
-const styleSheet = [
+const styleSheet: string | cytoscape.Stylesheet | cytoscape.Stylesheet[] = [
   {
     selector: 'node',
     style: {
-      backgroundColor: '#4a56a6',
-      width: 30,
-      height: 30,
+      backgroundColor: 'cyan',
+      width: 10,
+      height: 10,
       label: 'data(label)',
-      // "width": "mapData(score, 0, 0.006769776522008331, 20, 60)",
-      // "height": "mapData(score, 0, 0.006769776522008331, 20, 60)",
-      // "text-valign": "center",
-      // "text-halign": "center",
+      'text-valign': 'center',
+      'text-halign': 'center',
       'overlay-padding': '6px',
       'z-index': 10,
       //text props
       'text-outline-color': '#4a56a6',
-      'text-outline-width': '2px',
       color: 'white',
-      fontSize: 20,
+      'font-size': 6,
     },
   },
   {
@@ -64,27 +61,21 @@ const styleSheet = [
     style: {
       'border-width': '6px',
       'border-color': '#AAD8FF',
-      'border-opacity': '0.5',
-      'background-color': '#77828C',
-      width: 50,
-      height: 50,
+      'border-opacity': 0.5,
+      'background-color': 'red',
+      width: 30,
+      height: 30,
       //text props
       'text-outline-color': '#77828C',
-      'text-outline-width': 8,
+      'text-outline-width': 2,
     },
   },
-  {
-    selector: "node[type='device']",
-    style: {
-      shape: 'circle',
-    },
-  },
+
   {
     selector: 'edge',
     style: {
-      width: 3,
-      // "line-color": "#6774cb",
-      'line-color': '#AAD8FF',
+      width: 1,
+      'line-color': 'red',
       'target-arrow-color': '#6774cb',
       'target-arrow-shape': 'triangle',
       'curve-style': 'bezier',
